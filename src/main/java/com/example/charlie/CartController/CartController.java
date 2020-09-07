bfe05893fc49215afe13d13ee8ed3b6143374190
raivib <69424955+raivib@users.noreply.github.com>
2020-09-07 23:37:02 +0530

    Update CartController.java

--- src/main/java/com/example/charlie/CartController/CartController.java
+++ src/main/java/com/example/charlie/CartController/CartController.java
@@ -1,23 +1,45 @@
 package com.example.charlie.CartController;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.example.charlie.entities.Coupons;
+import com.example.charlie.entities.Products;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 
+public class CartController implements CommandLineRunner{
 
-public class CartController {
 
-    @GetMapping("/cart/NTB/INK")
-    String cart(){
-        return " Total amount for NTB and INK is Rs. 70";
+    @RequestMapping(value = "/cart/", method = RequestMethod.GET)
+    public static Object CallProduct() {
+        RestTemplate restTemplate = new RestTemplate();
+        Products producta = restTemplate.getForObject("http://coup-env.eba-fuy2s33u.ap-south-1.elasticbeanstalk.com/products/NTB", Products.class);
+        Products productb = restTemplate.getForObject("http://coup-env.eba-fuy2s33u.ap-south-1.elasticbeanstalk.com/products/INK", Products.class);
+        Coupons couponsc= restTemplate.getForObject("http://coup-env.eba-fuy2s33u.ap-south-1.elasticbeanstalk.com/coupons/7",Coupons.class);
+
+        int totalprice= producta.getPrice()+ productb.getPrice();
+        return Arrays.asList("Products:", producta.getProductCode(),productb.getProductCode(),"CouponCode", couponsc.getCouponCode(),"Total price is",totalprice,"Discounted price is",totalprice-(0.15*totalprice));
     }
-@GetMapping("/cart/NTB/INK/15OFF")
-    String total(){
-        return "Discounted amount for NTB and INK is Rs. 59.5";
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
 
 
-}
